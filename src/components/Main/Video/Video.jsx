5453f62e5a36caab4fc230831f50312d8c6082a7
MunFahim <muntasirf362@gmail.com>
2023-11-24 22:43:37 -0500

    updates video server

--- src/components/Main/Video/Video.jsx
+++ src/components/Main/Video/Video.jsx
@@ -21,7 +21,7 @@ function Video({ useSocket }) {
   useMemo(()=>{
     const peer = new Peer({
       host: 'u-meet-peerserver.onrender.com',
-      port: 3000,
+      port: 443,
       path: 'peerjs',
       secure: true
     })
